26bf7b4f758451e8cd0378d59acb4cb74fdbf608
Peksu <75335748+Pekk4@users.noreply.github.com>
2023-05-05 21:35:35 +0300

    Update dependencies & tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,18 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
-    "./src/Components/**/*.{js,jsx,ts,tsx}",
+
   ],
   theme: {
     extend: {
       colors: {
         'org-gray': '#555555',
+      },
+      fontWeight: {
+        'med': '550',
+      },
+      backgroundImage: {
+        'org-logo': "url('./assets/images/logo.png')",
       }
     },
   },
