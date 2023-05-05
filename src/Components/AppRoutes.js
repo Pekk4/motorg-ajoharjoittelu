b509a649f5c79ce406f427777e09c9eda8148f35
Peksu <75335748+Pekk4@users.noreply.github.com>
2023-05-05 21:27:02 +0300

    Add AppRoutes.js
    AppRoutes contains all routes used in application, e.g. /Kurssit etc.

---
+++ src/Components/AppRoutes.js
@@ -0,0 +1,30 @@
+import { Routes, Route } from "react-router-dom"
+
+import AjoHarjoittelu from "./AjoHarjoittelu"
+import Home from "./Home"
+import Kurssit from "./Kurssit"
+import Moottoriradat from "./Moottoriradat"
+import Tapahtumat from "./Tapahtumat"
+
+const Kauppa = () => {
+  window.location.replace("http://www.moottoripyora.org/kauppa");
+
+  return null;
+}
+
+const AppRoutes = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Ajoharjoittelu" element={<AjoHarjoittelu />} />
+        <Route path="/Kurssit" element={<Kurssit />} />
+        <Route path="/Tapahtumat" element={<Tapahtumat />} />
+        <Route path="/Moottoriradat" element={<Moottoriradat />} />
+        <Route path="/Kauppa" element={<Kauppa />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default AppRoutes
