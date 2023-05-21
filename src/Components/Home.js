32c501be241c1e659e0bca4e647052f7dd90e7aa
Peksu <75335748+Pekk4@users.noreply.github.com>
2023-05-21 19:31:28 +0300

    Fix broken links

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,8 +3,8 @@ const Home = () => (
     <p className="m-10">
       Tälle sivustolle on pyritty koostamaan kaikki oleellinen tieto MotOrg ry:n ajoharjoittelutapahtumista ns. yksiin kansiin.<br /><br />
       Kaiken ajoharjoitteluun liittyvän, kuten esimerkiksi säännöt ja erilaiset infot löydät osiosta <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="/Ajoharjoittelu">Ajoharjoittelu</a>.<br />
-      Kaiken Ajotaito-kursseihin liittyvän löydät puolestaan osiosta <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="/Ajoharjoittelu">Kurssit</a>.<br />
-      <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="/Ajoharjoittelu">Tapahtumat</a>-osiosta puolestaan löytyy kauden 2023 tapahtumat.
+      Kaiken Ajotaito-kursseihin liittyvän löydät puolestaan osiosta <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="/Kurssit">Kurssit</a>.<br />
+      <a className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="/Tapahtumat">Tapahtumat</a>-osiosta puolestaan löytyy kauden 2023 tapahtumat.
     </p>
     <img className="mx-auto my-10" src={require("../assets/images/welcome.jpg")} alt="#kneedown" />
     <p>
